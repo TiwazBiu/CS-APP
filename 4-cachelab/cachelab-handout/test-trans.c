8f20cf0e481d6b1f4653e1085f757c99897de40c
xiezhiwen <zhiwenxie1900@outlook.com>
2018-09-30 14:50:03 +0000

    customize zsh and git, fix some issue with locale

--- 4-cachelab/cachelab-handout/test-trans.c
+++ 4-cachelab/cachelab-handout/test-trans.c
@@ -65,13 +65,18 @@ void eval_perf(unsigned int s, unsigned int E, unsigned int b)
             results.funcid = i; /* remember which function is the submission */
 
 
-        printf("\nFunction %d (%d total)\nStep 1: Validating and generating memory traces\n",i,func_counter);
+        printf("\nFunction %d (%d total)\n"
+            "Step 1: Validating and generating memory traces\n",i,func_counter);
         /* Use valgrind to generate the trace */
 
-        sprintf(cmd, "valgrind --tool=lackey --trace-mem=yes --log-fd=1 -v ./tracegen -M %d -N %d -F %d  > trace.tmp", M, N,i);
+        sprintf(cmd, "valgrind --tool=lackey --trace-mem=yes --log-fd=1 -v "
+                "./tracegen -M %d -N %d -F %d  > trace.tmp", M, N,i);
         flag=WEXITSTATUS(system(cmd));
         if (0!=flag) {
-            printf("Validation error at function %d! Run ./tracegen -M %d -N %d -F %d for details.\nSkipping performance evaluation for this function.\n",flag-1,M,N,i);      
+            printf("Validation error at function %d! "
+                    "Run ./tracegen -M %d -N %d -F %d for details.\n"
+                    "Skipping performance evaluation for this function.\n"
+                    ,flag-1,M,N,i);      
             continue;
         }
 
